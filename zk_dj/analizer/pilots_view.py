fdeee2c54d388b412038ba1a2f0ebbe89cccdce2
despair1 <eveguide0@gmail.com>
2014-10-01 00:38:38 +0600

    add fetch pilot id from database

--- zk_dj/analizer/pilots_view.py
+++ zk_dj/analizer/pilots_view.py
@@ -7,18 +7,26 @@ Created on Sep 27, 2014
 from django.shortcuts import render
 from django.http import HttpResponse
 from eve_api import character_id
+from analizer.models import pilot
 
 def list_pilots(request):
     names_list=request.POST["names"].split("  ")
     t=[]
-    for i in names_list:
+    for name in names_list:
         d={}
-        d["name"]=i
+        d["name"]=name
         id1=0
-        pilots_list=character_id.get_id_by_pilot_name(i)
-        for i1 in pilots_list:
-            if i1["name"].upper()== i.upper():
-                id1=int(i1["characterID"])
+        pilots_list=pilot.objects.filter(name__iexact=name)
+        if len(pilots_list)==1:
+            id1=pilots_list[0].id
+        else:
+            pilots_list=character_id.get_id_by_pilot_name(name)
+            for i1 in pilots_list:
+                if i1["name"].upper()== name.upper():
+                    id1=int(i1["characterID"])
+                    pilot(name=name,id=id1).save()
+                    break
+                    
         if id1:
             d["id"]=id1
             t.append(d)
